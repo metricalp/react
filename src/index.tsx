04b6f27115aabdbec9a80c13fe39ff1c79d72467
metricalp <metricalpanalytics@gmail.com>
2023-12-26 13:25:36 +0300

    Metricalp react and nextjs integration lib first commit

---
+++ src/index.tsx
@@ -0,0 +1,75 @@
+import React, { useCallback } from 'react';
+import { useScript } from 'use-script';
+
+type MetricalpEvent = {
+  type: string;
+  [key: string]: string | number | boolean | undefined;
+};
+
+declare global {
+  interface Window {
+    metricalp: {
+      tid?: string;
+      version?: string;
+      queue?: MetricalpEvent[];
+      event: (e: MetricalpEvent) => void;
+    };
+  }
+}
+
+if (typeof window !== 'undefined') {
+  window.metricalp = window.metricalp || {
+    queue: [],
+    event: function (e: MetricalpEvent) {
+      this.queue?.push(e);
+    },
+  };
+}
+
+export const metricalpEvent = (e: MetricalpEvent) => {
+  if (typeof window === 'undefined') return;
+  window.metricalp?.event(e);
+};
+
+const SCRIPT_URL = 'https://cdn.metricalp.com/event/metricalp.js';
+
+export const METRICALP_SCREEN_VIEW_EV = 'screen_view';
+
+interface MetricalpProviderProps {
+  children?: any;
+  tid: string;
+  allowLocalhost?: boolean;
+  allowCustomElmEvents?: boolean;
+  disableAutoRouteCatch?: boolean;
+  hashRouting?: boolean;
+}
+
+export const MetricalpReactProvider: React.FC<MetricalpProviderProps> = ({
+  children = null,
+  tid,
+  allowLocalhost,
+  allowCustomElmEvents,
+  disableAutoRouteCatch,
+  hashRouting,
+}) => {
+  useScript(SCRIPT_URL, {
+    removeOnUnmount: false,
+    customAttributes: {
+      'data-tid': tid,
+      'data-allow-localhost': allowLocalhost ? 'true' : 'false',
+      'data-allow-custom-elm-events': allowCustomElmEvents ? 'true' : 'false',
+      'data-disable-auto-route-catch': disableAutoRouteCatch ? 'true' : 'false',
+      'data-hash-routing': hashRouting ? 'true' : 'false',
+    },
+  });
+
+  return children;
+};
+
+export const useMetricalp = () => {
+  const sendEvent = useCallback((e: MetricalpEvent) => {
+    metricalpEvent(e);
+  }, []);
+
+  return sendEvent;
+};
